2581faa1b3c0eefeda3512f37015d1fd691c1f51
manimohan <manimohan.t22@gmail.com>
2026-01-05 03:16:25 +0530

    functions updated

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u) #storing output of command "id -u" to a variable called USERID
+TIME_STAMP=$(date +%F-%H:%M:%S)
+SCRIPT_NAME=$(echo $0 |awk -F "." '{print $1}')
+#SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -19,11 +23,11 @@ else
     echo "Running the command as a root user"
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installation of MYSQL"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 
 VALIDATE $? "Installation of GIT"
 
@@ -32,3 +36,9 @@ VALIDATE $? "Installation of GIT"
 #Here, we created a VALIDATE function in which we are validating the variables $1 that is whether the exit code of previous run is 0 or not ($?)
 #And, vatiable $2, which is the operation that we are performing
 #We can call the function by simply giving the name as shown above
+#We are using the redirector to append the output to $LOGFILE
+# > by default it will redirect only success output
+# 1> it will ONLY redirect success output
+# 2> it will ONLY redirect error output
+# &> it will redirect both success & error output
+# &>> it will append both success & error output
