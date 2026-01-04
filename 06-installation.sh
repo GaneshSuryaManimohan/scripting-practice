eafbfbed653fa96be13d9343fce67f2efd94af40
manimohan <manimohan.t22@gmail.com>
2026-01-05 02:16:11 +0530

    installation script updated

--- 06-installation.sh
+++ 06-installation.sh
@@ -12,6 +12,24 @@ fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+    echo "Installation of MYSQL....FAILURE"
+    exit 1
+else
+    echo "Installation of MYSQL....SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of GIT....FAILURE"
+    exit 1
+else
+    echo "Installation of GIT....SUCCESS"
+fi
+
 echo "script is proceeding"
 
 
@@ -23,3 +41,4 @@ echo "script is proceeding"
 #When we run a valid command, then the exit code will be 0, else it be anything between 1-127
 #So, in the above installation, even if we are not running the script as root user an error is thrown, but the script will still proceed and print echo statements below
 #So, we are using exit code at the end of every step, so that if there's a failure at any step the script will not proceed further.
+#To check the exit code of previous run, we use special variable $?, if it return 0 then the script runs, else it stops as we are giving exit 1 if $? is not equal to 0
