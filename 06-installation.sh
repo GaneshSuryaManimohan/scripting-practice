815c9fbfd6ee2bf8a0f13ea383410e60ef84a306
manimohan <manimohan.t22@gmail.com>
2026-01-05 02:02:15 +0530

    installation script

--- 06-installation.sh
+++ 06-installation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+USERID=$(id -u) #storing output of command "id -u" to a variable called USERID
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this command as a root user"
+else
+    echo "Running the command as a root user"
+fi
+
 dnf install mysql -y
