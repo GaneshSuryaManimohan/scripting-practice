2913bb6540e36a8f873d8bc932f11c7515805950
manimohan <manimohan.t22@gmail.com>
2026-01-05 02:08:55 +0530

    installation script

--- 06-installation.sh
+++ 06-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u) #storing output of command "id -u" to a variable called USERID
 if [ $USERID -ne 0 ]
 then
     echo "Please run this command as a root user"
+    exit 1 #manually exiting the script incase of error
 else
     echo "Running the command as a root user"
 fi
@@ -12,3 +13,13 @@ fi
 dnf install mysql -y
 
 echo "script is proceeding"
+
+
+
+
+
+#In shell scripting, even in case of an error at a step, the script will proceed to execute to next steps.
+#To overcome this we are taking the exit codes into consideration
+#When we run a valid command, then the exit code will be 0, else it be anything between 1-127
+#So, in the above installation, even if we are not running the script as root user an error is thrown, but the script will still proceed and print echo statements below
+#So, we are using exit code at the end of every step, so that if there's a failure at any step the script will not proceed further.
