872ea4dc186ff9668405a52ba8c6941435241d1b
manimohan <manimohan.t22@gmail.com>
2026-01-05 01:18:19 +0530

    array.sh

--- 02.array.sh
+++ 02.array.sh
@@ -5,6 +5,7 @@ LIST=("Cricket" "Movies" "food")
 echo "First element of the list is:: ${LIST[0]}"
 echo "Second element of the list is:: ${LIST[1]}"
 echo "Third element of the list is:: ${LIST[2]}"
+echo "All elements from the list:: ${LIST[@]}"
 
 #here size of the list is 3, but the indexes are 0,1,2
 
