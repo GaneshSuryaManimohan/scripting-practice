093de745abcc53c855afbd8e63f5c89060bf2e7d
manimohan <manimohan.t22@gmail.com>
2026-01-05 01:05:50 +0530

    variables.sh

--- 01.variables.sh
+++ 01.variables.sh
@@ -9,3 +9,7 @@ echo "Please enter password::"
 read -s PASSWORD
 
 echo "username is $USERNAME and password is $PASSWORD"
+
+#here the read command takes the input provided and stores it as a variable USERNAME/PASSWORD
+# -s option will make sure the entered values are not displayed on terminal
+# at last, for verification purpose we are using echo to display the values stored in USERNAME and PASSWORD variables 
