98061f4c8b6f28fbb2c5ba2a40c35b02046f6f20
manimohan <manimohan.t22@gmail.com>
2026-01-05 03:57:28 +0530

    install package using loops

--- 08-install-packages.sh
+++ 08-install-packages.sh
@@ -6,4 +6,9 @@ for i in $@
 do
     echo "Package to Install: $i"
     dnf list installed $i
+    if [ $? -eq 0 ]
+    then
+        echo "$i is already installed....SKIPPING"
+    else
+        echo "$i needs to be installed"
 done
