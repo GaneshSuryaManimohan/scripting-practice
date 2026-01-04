51827c9e459d58a8a208edfe94dbe937d07c7fdc
manimohan <manimohan.t22@gmail.com>
2026-01-05 04:03:13 +0530

    install package using loops

--- 08-install-packages.sh
+++ 08-install-packages.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-#USERID=$(id -u)
+USERID=$(id -u)
+TIME_STAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 for i in $@
 do
     echo "Package to Install: $i"
-    dnf list installed $i
+    dnf list installed $i 
     if [ $? -eq 0 ]
     then
-        echo "$i is already installed....SKIPPING"
+        echo -e "$i is already installed.... $Y SKIPPING $N"
     else
         echo "$i needs to be installed"
     fi
