adcd33c8338a03156fb6578d5668289d141db246
manimohan <manimohan.t22@gmail.com>
2026-01-05 01:15:18 +0530

    array.sh

---
+++ 02.array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LIST ("Cricket" "Movies" "food")
+
+echo "First element of the list is:: ${LIST[0]}"
+echo "Second element of the list is:: ${LIST[1]}"
+echo "Third element of the list is:: ${LIST[2]}"
+
+#here size of the list is 3, but the indexes are 0,1,2
+
