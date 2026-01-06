8ef24d6bbafa0118e8272bb111660b96fd71c97c
manimohan <manimohan.t22@gmail.com>
2026-01-06 18:59:12 +0530

    Updated script 09

--- 09-current-script.sh
+++ 09-current-script.sh
@@ -21,8 +21,16 @@ echo "Other script is called here and name from other script is:: $NAME"
 # Process ID of this script is:: 1837
 # Other script is called here and name from other script is:: GANESH SURYA MANIMOHAN
 
-# In this method the variable values will not change after calling other script
+# In this method the variable values are not changed after calling other script
 # PIDs of two scripts are also different
 
 # Output Method2: source ./10-other-script.sh
+# sh 09-current-script.sh
+# Name from current script is:: GANESH SURYA MANIMOHAN
+# Process ID of the current script is:: 5341
+# Name from the other script is:: THUTTAGUNTA
+# Process ID of this script is:: 5341
+# Other script is called here and name from other script is:: THUTTAGUNTA
 
+# In this method the variable values are changed after calling other script
+# PIDs of two scripts are same
