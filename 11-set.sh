5882cbaebcdab73e59a7802ece63aab4c667205f
manimohan <manimohan.t22@gmail.com>
2026-01-06 19:43:22 +0530

    created set.sh

---
+++ 11-set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+USERID=$(id -u) #storing output of command "id -u" to a variable called USERID
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this command as a root user"
+    exit 1 #manually exiting the script incase of error
+else
+    echo "Running the command as a root user"
+fi
+
+dnf install mysql -y
+dnf install git -y
+echo "script is proceeding"
+
+# Instead of writing VALIDATE everytime after a step, we can use set -e command. The shell script will stop executing the script in case of any error without proceeding to next steps.
