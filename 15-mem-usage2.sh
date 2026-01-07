f5f0bc071ef1c638295f127eb55090580c9f70bc
manimohan <manimohan.t22@gmail.com>
2026-01-07 15:36:52 +0530

    mem-usage2 created

--- 15-mem-usage2.sh
+++ 15-mem-usage2.sh
@@ -2,7 +2,7 @@
 
 MEM_USAGE=$(free -mh)
 
-while
+while IFS= read -r line
 do
     USAGE=$(echo $line | awk 'NR==1 {h1=$1; h2=$2; h3=$3; h4=$NF} NR==2 {print h1 ":" $2 ,h2 ":" $3, h3 ":" $4, h4 ":" $NF}')
     MSG+="$USAGE"
