96b596fd22240b79315401c833d1f9b26ec231c5
manimohan <manimohan.t22@gmail.com>
2026-01-07 15:38:32 +0530

    mem-usage2 created

--- 15-mem-usage2.sh
+++ 15-mem-usage2.sh
@@ -7,4 +7,4 @@ do
     USAGE+=$(echo $line | awk 'NR==1 {h1=$1; h2=$2; h3=$3; h4=$NF} NR==2 {print h1 ":" $2 ,h2 ":" $3, h3 ":" $4, h4 ":" $NF}')
 done <<< $MEM_USAGE
 
-echo -e "Current RAM on Server::  $MSG"
+echo -e "Current RAM on Server::  $USAGE"
