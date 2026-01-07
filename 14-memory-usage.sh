a1b9412e5829077523341593343374f16a1b55e5
manimohan <manimohan.t22@gmail.com>
2026-01-07 14:54:29 +0530

    memory usage script added

---
+++ 14-memory-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MEM_USAGE=$(free -m |grep Mem )
+MEM_THR=100
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line |awk -F " " '{print $3F}')
+    AVAIL=$(echo $line |awk -F " " '{print $NF}')
+    if [ $USAGE -ge $MEM_THR ]
+    then
+        echo "Memory Usage $USAGE is more than $MEM_THR, free space available is $AVAIL"
+    fi
+done
