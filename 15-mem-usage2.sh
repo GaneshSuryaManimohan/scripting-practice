61e0d378b82a455602859369e295692af3962b65
manimohan <manimohan.t22@gmail.com>
2026-01-07 15:47:06 +0530

    mem-usage2 created

--- 15-mem-usage2.sh
+++ 15-mem-usage2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MEM_USAGE=$(free -m | awk 'NR==1 {h1=$1; h2=$2; h3=$3; h4=$NF} NR==2 {print h1 ":" $2 ,h2 ":" $3, h3 ":" $4, h4 ":" $NF}')
+MAIL="Current memory Usage on server $HOSTNAME is $MEM_USAGE"
+#echo "Current RAM on Server $HOSTNAME:: $MEM_USAGE"
 
-echo "Current RAM on Server $HOSTNAME:: $MEM_USAGE"
-
-echo $MEM_USAGE | mail -s "Memory Utilization on Server"  thuttagunta.suryamanimohan@gmail.com
+echo $MAIL | mail -s "Memory Utilization on Server"  thuttagunta.suryamanimohan@gmail.com
