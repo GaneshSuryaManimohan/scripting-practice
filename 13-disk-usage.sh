285e0f4118e07ea8dfbaba2b4b2dbe3629f206e1
manimohan <manimohan.t22@gmail.com>
2026-01-07 13:05:51 +0530

    disk usage script updated

--- 13-disk-usage.sh
+++ 13-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT |grep xfs)
 DISK_THRESHOLD=30
+MSG=""
 
 while IFS= read -r line
 do
@@ -9,6 +10,8 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$FOLDER usage is more than $DISK_THRESHOLD, current usage is $USAGE"
+        MSG="$FOLDER usage is more than $DISK_THRESHOLD, current usage is $USAGE"
     fi
 done <<< $DISK_USAGE
+
+echo "Message:: $MSG"
